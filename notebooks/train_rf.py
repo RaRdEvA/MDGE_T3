a8f647c2c0d1acdd68170bc6bdd3319cf12e9dae
RaRdEvA <rardeva@yahoo.com>
2024-02-10 17:57:50 -0600

    updated readme and refactor scripts

--- notebooks/train_rf.py
+++ notebooks/train_rf.py
@@ -15,18 +15,18 @@ from scripts import train_model
 
 if __name__ == "__main__":
     # Directorios de entrada y salida
-    input_dir = "../data/prep"
-    output_dir = "../models"
+    INPUT_DIR = "../data/prep"
+    OUTPUT_DIR = "../models"
 
     # Asegurarse de que la carpeta de salida exista, si no, crearla
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
 
     # Listar archivos en la carpeta de entrada
-    input_files = os.listdir(input_dir)
+    input_files = os.listdir(INPUT_DIR)
 
     # Entrenar el modelo para cada archivo .csv en la carpeta de entrada
     for file in input_files:
         if file.endswith(".csv"):  # Solo procesar archivos CSV
-            input_file_path = os.path.join(input_dir, file)
-            train_model(input_file_path, output_dir)
+            input_file_path = os.path.join(INPUT_DIR, file)
+            train_model(input_file_path, OUTPUT_DIR)
