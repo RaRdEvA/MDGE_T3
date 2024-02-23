79a37a65d3aa14e5d331d370c44cc9d35fe9d9c3
RaRdEvA <rardeva@yahoo.com>
2024-02-23 02:10:07 -0600

    updated test

--- test_utils.py
+++ test_utils.py
@@ -8,17 +8,16 @@ def test_setup_logger_creates_file():
     '''
     Prueba que setup_logger crea un archivo de log correctamente.
     '''
-    logger_name = "test_logger"
-    logger = setup_logger(logger_name, 2)
+    logger = setup_logger("test_logger", 2)
     logger.info(f"Prueba")
-    assert os.path.exists(f'./logs/{logger_name}_*.log')
+    assert os.path.exists(f'./logs/test_logger_*.log')
 
 
 def test_setup_logger_level_debug():
     '''
     Prueba que el logger se configura en nivel DEBUG correctamente.
     '''
-    logger = setup_logger("debug_logger", 2)
+    logger = setup_logger("test_debug_logger", 2)
     assert logger.level == logging.DEBUG
 
 
@@ -26,7 +25,7 @@ def test_setup_logger_level_info():
     '''
     Prueba que el logger se configura en nivel INFO correctamente.
     '''
-    logger = setup_logger("info_logger", 1)
+    logger = setup_logger("test_info_logger", 1)
     assert logger.level == logging.INFO
 
 
@@ -34,7 +33,7 @@ def test_setup_logger_level_error():
     '''
     Prueba que el logger se configura en nivel ERROR correctamente.
     '''
-    logger = setup_logger("error_logger", 3)
+    logger = setup_logger("test_error_logger", 3)
     assert logger.level == logging.ERROR
 
 
@@ -42,6 +41,6 @@ def test_log_file_format():
     '''
     Prueba que el archivo de log sigue el formato de nombre esperado.
     '''
-    logger = setup_logger("format_test", 2)
+    logger = setup_logger("test_format_test", 2)
     files = os.listdir('./logs')
     assert any("format_test_" in file for file in files)
